726e2a3ba659d5c557c469f81c78a0b25f96f286
Sun Tingyang <tfs@5679.com>
2024-04-12 15:04:29 -0400

    tmp

--- run_training.sh
+++ run_training.sh
@@ -20,14 +20,14 @@ MIXING_MATRICES=(
     # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_IAB_CIFAR10_SCA23_1.pkl"
     # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_IAB_CIFAR10_BoydGreedy_2.pkl"
     # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_IAB_CIFAR10_BoydGreedy_1.pkl"
-    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_clique.pkl"
-    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_prim.pkl"
-    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_ring.pkl"
-    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_random.pkl"
-    "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_BoydGreedy_1.pkl"
-    "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_SCA23_1.pkl"
-    "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_SDRLambda2Ew_1.pkl"
-    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_SDRRhoEw_1.pkl"
+    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_clique.pkl"
+    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_prim.pkl"
+    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_ring.pkl"
+    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_random.pkl"
+    "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_BoydGreedy_1.pkl"
+    "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_SCA23_1.pkl"
+    "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_SDRLambda2Ew_1.pkl"
+    # "/scratch2/tingyang/DFS_Topo/mixing_matrix/mixing_matrix_Roofnet_CIFAR10_SDRRhoEw_1.pkl"
 )
 
 # Define an array with the GPU IDs you want to use
